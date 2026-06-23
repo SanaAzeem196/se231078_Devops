b4f6b816bcc1366bbbef77c7af3cb00ed03f6678
SanaAzeem196 <sanaazeem263@gmail.com>
2026-06-23 14:08:01 +0500

    m

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
     if(req.url==='/'){
-        res.write('Hello World from jenkins pipeline');
+        res.write('Hello World from jenkins');
         res.end();
     }
 });
