6bd8ecb2089969eaf1e37195e2cdace96ab4a6b7
SanaAzeem196 <sanaazeem263@gmail.com>
2026-06-23 22:11:11 +0500

    m

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http=require('http');
 const server=http.createServer((req,res)=>{
     if(req.url==='/'){
-        res.write('Hello World from lecture 12+++');
+        res.write('Hello World from lecture 12++++');
         res.end();
     }
 });
